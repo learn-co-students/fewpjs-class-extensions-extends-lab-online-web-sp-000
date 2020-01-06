4bc9f77a56dc39efa5444836043733e15ddb451b
stabassum <sumu1993@gmail.com>
2020-01-05 21:24:36 -0500

    Done.

--- index.js
+++ index.js
@@ -2,16 +2,23 @@
 
 class Polygon{
 
-    constructor(array){
-        this.array = array;
+    constructor(sides){
+        this.sides = sides;
+        this.count = this.sides.length;
     }
 
     get countSides(){
-     
+        return this.sides.length;
     }
 
     get perimeter(){
-
+        if (!Array.isArray(this.sides)) return;
+        let sum = 0;
+        for (var int of this.sides) {
+          sum += int
+        }
+        return sum
+      }
     }
 
 }
