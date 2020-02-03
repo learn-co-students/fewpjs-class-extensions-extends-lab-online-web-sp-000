40bddd4807d0a27d6fdcda19d1bcd4993290e511
Bernadette Davis <bsd231@nyu.edu>
2020-02-03 23:49:12 +0000

    Done.

--- index.js
+++ index.js
@@ -19,16 +19,31 @@ class Polygon{
 
 class Triangle extends Polygon{
   get isValid(){
-    if (this.countSides === 3){
-      if (this.sides[0] + this.sides[1] > this.sides[2]){
-        return false;
-      } else if (this.sides[1] + this.sides[2] > this.sides[0]){
-        return false;
-      } else if (this.sides[2] + this.sides[0] > this.sides[1]){
-        return false;
-      }
+    
+    if (!(Array.isArray(this.sides))) return;
+    if (this.countSides !== 3) return;
+    
+    if ((this.sides[0] + this.sides[1]) > this.sides[2] && (this.sides[1] + this.sides[2]) > this.sides[0] && (this.sides[2] + this.sides[0]) > this.sides[1])
       return true;
-    }
+      
+      return false;
+    
   }
 }
-class Square extends Polygon{}
+class Square extends Polygon{
+  get area(){
+    return this.sides[0] * this.sides[0];
+  }
+  
+  get isValid(){
+    
+    if (!(Array.isArray(this.sides))) return;
+    if (this.countSides !== 4) return;
+    
+    if (this.sides[0] === this.sides[1] && this.sides[1] === this.sides[2] && this.sides[2] === this.sides[3])
+      return true;
+      
+      return false;
+    
+  }
+}
