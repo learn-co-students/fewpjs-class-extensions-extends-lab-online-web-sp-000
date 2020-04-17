d0f7803b9dbb30d1fd24cf05292931bff16c2678
Ronald Stephens <rstephens@bluemorpho.cc>
2020-04-17 17:15:54 -0400

    Done.

--- index.js
+++ index.js
@@ -5,44 +5,39 @@ class Polygon {
     }
 
     get countSides() {
-        console.log(`sides: ${this.array.length}`)
         return this.array.length
     }
 
     get perimeter() {
         return this.array.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
     }
-
 }
 
 class Triangle extends Polygon {
 
     get isValid() {
-        if (this.array.length === 3) {
-            const sideA = this.array[0]
-            const sideB = this.array[1]
-            const sideC = this.array[2]
+        if (!Array.isArray(this.array)) return
+        if (this.array.length != 3) return
 
-           return (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
-        } else {
-            return false
-        }
-    }
+        const sideA = this.array[0]
+        const sideB = this.array[1]
+        const sideC = this.array[2]
+
+        return ((sideA + sideB > sideC) && (sideA + sideC > sideB) && (sideB + sideC > sideA))
+}
 }
 
 class Square extends Polygon {
 
     get isValid() {
-        if (this.array.length === 4) {
+        if (!Array.isArray(this.array)) return
+        if (this.array.length != 4) return
             const sideA = this.array[0]
             const sideB = this.array[1]
             const sideC = this.array[2]
             const sideD = this.array[3]
 
-            return (sideA === sideB && sideB === sideC && sideC === sideD)
-        } else {
-            return false
-        }
+        return ((sideA === sideB) && (sideB === sideC) && (sideC === sideD))
     }
 
     get area() {
