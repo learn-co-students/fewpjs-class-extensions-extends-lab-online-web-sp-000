74a852c4691fbedbf5c086bda81d652a3b6a10d0
Alvin Coulson <aaacoulson@gmail.com>
2020-08-18 18:36:38 -0400

    Done.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ class Triangle extends Polygon {
 
 class Square extends Polygon {
     get isValid(){
-      if(this.sides[0] === this.sides[1] && this.sides[2] === this.sides[3])
+      if(this.sides[0] === this.sides[1] && this.sides[1] === this.sides[2] && this.sides[2] === this.sides[3])
         { return true;
       } else {
         return false;
