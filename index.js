48a817f1b83e8c0f80ead01de62851fccc77fcee
ottocedeno <otto@creativespaceclub.com>
2020-08-24 19:21:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+class Polygon {
+  constructor(arrayOfSides) {
+    this.sides = arrayOfSides;
+  }
+
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    return this.sides.reduce((acc, side) => acc + side, 0);
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    const [a, b, c] = this.sides;
+    return !!(a + b > c && b + c > a && a + c > b);
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    return this.sides.every((side) => side === this.sides[0]);
+  }
+
+  get area() {
+    return this.sides[0] ** 2;
+  }
+}
