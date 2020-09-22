0f00bc87b6ebb92bc9840d7757bc13d54752b1ce
connorworthen <connorw220@protonmail.com>
2020-09-21 22:37:15 -0700

    Done.

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ class Triangle extends Polygon {
   get isValid() {
     let triangle1 = this.sides;
     let triangle2 = this.sides;
+    let triangle3 = 0;
     for (i < 0; i < this.sides.length; i++) {
-      if (triangle1 + triangle2 > this.sides) return;
+      if (triangle1 + triangle2 < triangle3) return true;
     }
   }
 }
