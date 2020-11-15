9f9f6f5aa1016b96d9b89be108c8814997ff9227
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-15 05:05:33 +0000

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 class Polygon {
   constructor(sides) {
     this.sides = sides;
+    this.count = this.sides.length;
   }
 
   get countSides() {
@@ -15,18 +16,29 @@ class Polygon {
 
 class Triangle extends Polygon {
   get isValid() {
-    if (this.sides[0] + this.sides[1] > this.sides[2]) {
-      true;
-    } else if (this.sides[0] + this.sides[2] > this.sides[1]) {
-      true;
-    } else if (this.sides[2] + this.sides[1] > this.sides[0]) {
-      true;
-    } else {
-      false;
-    }
+    if (!Array.isArray(this.sides)) return;
+    if (this.count !== 3) return;
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
   }
 }
 
 class Square extends Polygon {
+  get isValid() {
+    if (!Array.isArray(this.sides)) return;
+    if (this.count !== 4) return;
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    let side4 = this.sides[3]
+    return ((side1 === side2) && (side1 === side3) && (side1 === side4))
+  }
 
+  get area() {
+    if (!Array.isArray(this.sides)) return;
+    if (this.count !== 4) return;
+    return this.sides[0] * this.sides[0]
+  }
 }
