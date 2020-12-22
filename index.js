071fe641e23b7c754d0f658ad35dc0389754dbcd
mlgvla <mlgerardvla@gmail.com>
2020-12-22 18:52:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+class Polygon {
+    constructor(array) {
+        this.intArray = array
+    }
+
+    get countSides() {
+        return this.intArray.length
+    }
+
+    get perimeter() {
+        return this.intArray.reduce((memo, num) => memo + num, 0)
+    }
+}
