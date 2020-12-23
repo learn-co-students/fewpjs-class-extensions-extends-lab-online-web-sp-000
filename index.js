06df92144227f0ab98e0dd75150f2e8bd7c92a1d
mlgvla <mlgerardvla@gmail.com>
2020-12-22 21:10:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 class Polygon {
+
     constructor(array) {
         this.intArray = array
     }
@@ -9,5 +10,41 @@ class Polygon {
 
     get perimeter() {
         return this.intArray.reduce((memo, num) => memo + num, 0)
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid(){
+        
+        let [a, b, c] = this.intArray
+
+        if (a + b > c &&  b + c > a && a + c > b) {
+            return true
+        }
+        return false
+    }
+}
+
+class Square extends Polygon {
+ 
+
+    get isValid(){ 
+
+        let [a, b, c, d] = this.intArray
+
+        if (a == b && a == c && a == d && b == c && b == d && c == d) {
+            return true
+        }
+        return false
+    }
+
+    get area(){
+        
+        if (this.isValid) {
+            let [a, b] = this.intArray
+            return a * b
+        }
+        return "This is not a valid square"        
     }
 }
