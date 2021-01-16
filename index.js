5ec2719a1529d38725d19131b100137d621ea055
CoderJay06 <jaycru06@yahoo.com>
2021-01-15 23:59:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+class Polygon {
+    constructor(sidesArray) {
+            this.sidesArray = sidesArray;
+        }
+        // 1) Polygon has a countSides getter method that returns the number of sides of the polygon
+    get countSides() {
+            return this.sidesArray.length;
+        }
+        //  2) Polygon has a perimeter getter that calculates perimeter
+    get perimeter() {
+        return this.sidesArray.reduce((acc, currSide) => acc + currSide);
+    }
+}
+
+
+class Triangle extends Polygon {
+    constructor(sidesArray) {
+            super(sidesArray);
+        }
+        // 3) checks for valid triangle
+    get isValid() {
+        if (this.triangleHasValidSides()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    triangleHasValidSides() {
+            return (this.sidesArray[0] + this.sidesArray[1] > this.sidesArray[2] &&
+                this.sidesArray[0] + this.sidesArray[2] > this.sidesArray[1] &&
+                this.sidesArray[1] + this.sidesArray[2] > this.sidesArray[0]);
+        }
+        // 4) has a perimeter getter inherited from Polygon
+}
+
+// Square
+// 5) has a Square class
+// 6) has a perimeter getter inherited from Polygon
+// 7) calculates the area
+// 8) checks for valid square
