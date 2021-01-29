ecf89d39fa594ebb1971877b962bce7282085b0a
learn-co-git <argus.two.2@protonmail.com>
2021-01-29 13:34:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Polygon {
+  constructor(array) {
+    this.array = array
+  }
+
+  get countSides() {
+    return this.array.length
+    }
+
+  get perimeter() {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return this.array.reduce(reducer, 0)
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if ((this.countSides === 3) && ((this.array[0] === this.array[1]) || (this.array[1] === this.array[3]) || (this.array[3] === this.array[0]))) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    return this.array.every(x => x === this.array[0])
+  }
+
+  get area() {
+    return this.array[0] ** 2
+  }
+}
