15e3e8df046bafcca498c8e238eae56c30316cee
Jake <jake098877@gmail.com>
2021-03-02 22:40:45 -0500

    Done.

--- index.js
+++ index.js
@@ -2,26 +2,39 @@ class Polygon {
   constructor(arr) {
     let i = 1;
     for (let s of arr) {
-      this.side`${i}` = s
+      this[`side${i}`] = s
       i++;
     }
   }
 
   get countSides() {
-    return arr.length;
+    return Object.keys(this).length;
   }
 
   get perimeter() {
-    return arr.reduce(total, curr => total + curr )
+    return Object.values(this).reduce((total, curr) => total + curr);
   }
 }
 
 class Triangle extends Polygon {
   get isValid() {
     if (this.countSides === 3) {
-      
+      if (Object.values(this)[0] + Object.values(this)[1] > Object.values(this)[2] && Object.values(this)[0] + Object.values(this)[2] > Object.values(this)[1] && Object.values(this)[2] + Object.values(this)[1] > Object.values(this)[0])
         return true;
     }
     return false;
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (Object.values(this)[0] === Object.values(this)[1] && Object.values(this)[2] === Object.values(this)[3] && Object.values(this)[0] === Object.values(this)[3]) {
+      return true;
+    }
+    return false;
+  }
+  
+  get area() {
+    return Object.values(this)[0] ** 2
   }
 }
