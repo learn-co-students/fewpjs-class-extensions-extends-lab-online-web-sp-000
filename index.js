10dd5b54a8076413543372c259e79ee11d0c9063
Chandra Lama <iamme24cl@gmail.com>
2021-03-09 20:36:17 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+class Polygon {
+  constructor(sides) {
+    this.sides = sides;    
+  }
+  get countSides() {
+    return this.sides.length;
+  }
+
+  get perimeter() {
+    let sum = 0;
+    for (let int of this.sides) {
+      sum += int;
+    }
+    return sum;
+  }
+}
